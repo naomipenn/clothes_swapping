f6036b3b77813b5d9d5d4d1ec18c1331cdec8c30
Russell Yeo <rusyeo@gmail.com>
2017-02-01 18:19:17 +0000

    tests still failing

--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -1,5 +1,5 @@
 def add_an_item
-  # user = User.create(email: 'test@test.com')
-  @item = Item.new(size: 'XS', color: 'Orange', category: 'Onesie', description: 'Pokemon onesie')
+  @user = User.create(email: 'test@test.com')
+  @item = Item.new(size: 'XS', color: 'Orange', category: 'Onesie', description: 'Pokemon onesie', user: @user)
   @item.save!
 end
