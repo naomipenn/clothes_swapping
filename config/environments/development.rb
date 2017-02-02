c9173460c8909d95b3f08b4af385e9efa11cac85
Russell Yeo <rusyeo@gmail.com>
2017-02-02 14:03:22 +0000

    extract aws bucket var to env.yml file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,7 +55,7 @@ Rails.application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_region => 'eu-west-2',
-    :bucket => 'clothes-swapping',
+    :bucket => ENV.fetch('AWS_BUCKET'),
     :s3_credentials => {:access_key_id => ENV.fetch('AWS_ACCESS_KEY_ID'),
                         :secret_access_key => ENV.fetch('AWS_SECRET_ACCESS_KEY')}
   }
