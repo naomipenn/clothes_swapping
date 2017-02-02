1d08ac2199418b7617ed7ee599eab6787f2c7ec8
Russell Yeo <rusyeo@gmail.com>
2017-02-02 09:51:05 +0000

    add image field to item entry

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -69,6 +69,6 @@ class ItemsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def item_params
-      params.require(:item).permit(:size, :color, :type)
+      params.require(:item).permit(:size, :color, :type, :image)
     end
 end
