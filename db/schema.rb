f588cbdbe4b5a28db7922a40dfdbddb762c2e3fc
Jack <jack_6894@hotmail.com>
2017-02-02 10:41:32 +0000

    thursday intial pull, gemfile and schema updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170201180845) do
+ActiveRecord::Schema.define(version: 20170202093011) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,13 +19,13 @@ ActiveRecord::Schema.define(version: 20170201180845) do
     t.string   "size"
     t.string   "color"
     t.string   "category"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "description"
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.string   "description"
     t.integer  "user_id"
     t.index ["user_id"], name: "index_items_on_user_id", using: :btree
   end
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 20170201180845) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "provider"
+    t.string   "uid"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
